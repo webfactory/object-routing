5f80d415dcd35147fb689987af7d47d7c5dc3a51
Matthias Pigulla <mp@webfactory.de>
2024-04-04 22:18:31 +0200

    Add a test for the AttributeDriver

---
+++ tests/JMS/Tests/ObjectRouting/Metadata/Driver/Fixture/BlogPostWithAttributes.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace JMS\Tests\ObjectRouting\Metadata\Driver\Fixture;
+
+use JMS\ObjectRouting\Annotation\ObjectRoute;
+
+#[ObjectRoute(type: "view", name: "blog_post_view", params: ['slug' => 'slug'])]
+#[ObjectRoute(type: "edit", name: "blog_post_edit", params: ['slug' => 'slug'])]
+class BlogPostWithAttributes
+{
+    private $slug;
+
+    public function __construct($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+}
